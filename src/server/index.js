455873766e1315afe758c6bfd0e255a3bc75686e
Veda <pal.veda.deepta@gmail.com>
2018-08-30 01:39:07 +0530

    dynamic port for heroku added

--- src/server/index.js
+++ src/server/index.js
@@ -2,12 +2,12 @@ const express = require('express')
 const os = require('os')
 
 const app = express()
+app.set('port', (process.env.PORT || 8080))
 
 app.use(express.static('dist'))
-app.get('/api/getUsername', (req, res) =>
-  res.send({ username: os.userInfo().username })
-)
+
 app.get('*', (req, res) => {
   res.sendStatus(200)
 })
-app.listen(8080, () => console.log('Listening on port 8080!'))
+
+app.listen(app.get('port'), () => console.log('Listening on port 8080!'))
