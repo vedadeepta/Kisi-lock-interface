7a60a61223edb10ea5a0f3878bfa48d53bc9c940
Veda <pal.veda.deepta@gmail.com>
2018-08-30 01:47:27 +0530

    server fixes

--- src/server/index.js
+++ src/server/index.js
@@ -1,12 +1,13 @@
 const express = require('express')
-
+const path = require('path')
 const app = express()
 app.set('port', (process.env.PORT || 8080))
 
-app.use(express.static(__dirname + '/dist'))
+app.use(express.static(path.join(__dirname, '../../dist')))
 
 app.get('*', (req, res) => {
-  res.sendStatus(200)
+
+  res.sendFile(path.join(__dirname, '../../dist/index.html'))
 })
 
 app.listen(app.get('port'), () => console.log('Listening on port 8080!'))
