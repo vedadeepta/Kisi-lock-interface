46836143cfe0d98224d057d009cebf7ba92a22c8
Veda <pal.veda.deepta@gmail.com>
2018-08-30 01:24:00 +0530

    Server files

--- src/server/index.js
+++ src/server/index.js
@@ -3,12 +3,11 @@ const os = require('os')
 
 const app = express()
 
-// app.use(express.static('dist'))
+app.use(express.static('dist'))
 app.get('/api/getUsername', (req, res) =>
   res.send({ username: os.userInfo().username })
 )
 app.get('*', (req, res) => {
-  console.log('here')
   res.sendStatus(200)
 })
 app.listen(8080, () => console.log('Listening on port 8080!'))
